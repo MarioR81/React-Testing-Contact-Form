15bdedde86e5c80a0cc5e055f10aaee35f33caa3
apple <apple@apples-MacBook-Pro-2.local>
2020-02-13 20:32:16 -0800

    periodic save

--- src/App.test.js
+++ src/App.test.js
@@ -9,4 +9,13 @@ test("renders App without crashing", () => {
   const header = getByText(/testing/i);
   // Assert
   expect(header).toBeInTheDocument();
+  expect(header).toBeTruthy();
+  expect(header).not.toBeFalsy();
 });
+
+// jest global function
+//- test
+// test('test concise', () => {
+//  const { getByText } = render(<App />);
+//  getByText(/testing/i);
+// });
